b81f134712986582dbed69ec9c4e36b8c69c0ce4
Toshiaki Maki <tmaki@pivotal.io>
2019-01-24 02:46:42 +0900

    Spring Boot version

--- setup-flyway.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.1.4/flyway-commandline-5.1.4-linux-x64.tar.gz
-tar xzvf flyway-commandline-5.1.4-linux-x64.tar.gz
-mv flyway-5.1.4/ flyway
-
-for f in `find atrs-initdb/src/sqls/integration-test-postgres -type f`;do 
-  cp $f ./flyway/sql/V`basename $f | sed 's/_/__/'`;
-done
---
+++ src/main/java/jp/co/ntt/atrs/config/AppConfig.java
@@ -0,0 +1,38 @@
+package jp.co.ntt.atrs.config;
+
+import java.io.IOException;
+
+import org.dozer.spring.DozerBeanMapperFactoryBean;
+import org.terasoluna.gfw.common.date.jodatime.DefaultJodaTimeDateFactory;
+import org.terasoluna.gfw.common.date.jodatime.JodaTimeDateFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@ImportResource(locations = "classpath:spring/codelist.xml")
+public class AppConfig {
+
+	@Bean
+	public JodaTimeDateFactory dateFactory() {
+		return new DefaultJodaTimeDateFactory();
+	}
+
+	@Bean
+	public DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean(
+			ResourcePatternResolver resolver) throws IOException {
+		DozerBeanMapperFactoryBean factoryBean = new DozerBeanMapperFactoryBean();
+		factoryBean.setMappingFiles(
+				resolver.getResources("classpath:/dozer/**/*-mapping.xml"));
+		return factoryBean;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
